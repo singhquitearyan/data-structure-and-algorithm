99d78cc73d7a7dd656b11426d35583a63f3c43f4
singhquitearyan <152776855+singhquitearyan@users.noreply.github.com>
2025-09-13 21:55:03 +0530

    Create solution.cpp

---
+++ Recursion/001-print-1-to-n/solution.cpp
@@ -0,0 +1,29 @@
+// Author: Aryan SIngh
+// Date: 13/09/25
+//description: Printin ! to N using recursion
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void func(int i, int n){
+   
+   // Base Condition.
+   if(i>n) return;
+   cout<<i<<endl;
+
+   // Function call to print i till i increments to n.
+   func(i+1,n);
+
+}
+
+int main(){
+  
+  // Here, let’s take the value of n to be 4.
+  int n = 4;
+  func(1,n);
+  return 0;
+
+}
