f1877484e1e5b9699f328768dbc0b72d2c09ca37
singhquitearyan <152776855+singhquitearyan@users.noreply.github.com>
2025-11-09 21:33:07 +0530

    Update solution.cpp

--- Linked Lists/011-reverse-a-DLL/solution.cpp
+++ Linked Lists/011-reverse-a-DLL/solution.cpp
@@ -1,1 +1,31 @@
+//Author: Aryan SIngh
+//Date: 09/11/25
+//Description:
 
+class Node {
+  public:
+    int data;
+    Node *next;
+    Node *prev;
+    Node(int val) {
+        data = val;
+        next = NULL;
+        prev = NULL;
+    }
+};
+class Solution {
+  public:
+    Node *reverse(Node *head) {
+        // code here
+        if( head == NULL || head -> next == NULL) return head;
+        Node* prev = NULL;
+        Node* current = head;
+        while(current != NULL){
+            prev = current -> prev;
+            current -> prev = current -> next;
+            current -> next = prev;
+            current = current -> prev;
+        }
+        return prev -> prev;
+    }
+};
