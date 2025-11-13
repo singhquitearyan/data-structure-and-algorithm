f349e390b4f50cc54da7a302e32e8192a9882602
singhquitearyan <152776855+singhquitearyan@users.noreply.github.com>
2025-11-13 18:58:36 +0530

    Create solution.cpp

---
+++ Linked Lists/022-starting-node-in-loop-in-LL/solution.cpp
@@ -0,0 +1,33 @@
+//Author: Aryan Singh
+//Date: 13/11/25
+//Description:
+  Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+  };
+ 
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        
+        if( head == NULL || head -> next == NULL) return NULL;
+        while(fast != NULL && fast -> next != NULL){
+            slow = slow -> next;
+            fast = fast -> next -> next;
+            if( fast == slow)
+            break;
+        }
+        if(fast == NULL || fast -> next == NULL) return NULL;
+        slow = head;
+        while(slow != fast){
+            slow = slow -> next;
+            fast = fast -> next;
+        }
+        return slow;
+    }
+
+};
