37a2348e7dc3cc00bc99744260fecf285a1a7882
singhquitearyan <152776855+singhquitearyan@users.noreply.github.com>
2025-11-30 19:22:37 +0530

    Create solution.cpp

---
+++ Stack & Queues/018-next-greater-element-ll/solution.cpp
@@ -0,0 +1,27 @@
+//Author: Aryan SIngh
+//Date: 30/11/25
+//Description:
+
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        stack<int> st;
+        int n = nums.size();
+        vector<int> res(n);
+
+        for( int i = 2*n-1;  i >= 0; i--){
+            while( !st.empty() && st.top() <= nums[i % n ] ){
+                st.pop();
+            }
+            if(i<n){
+
+            if(st.empty()) res[i] = -1;
+            else res[i] = st.top();
+            
+            }
+            st.push(nums[i % n]);
+        }
+        return res;
+    
+    }
+};
