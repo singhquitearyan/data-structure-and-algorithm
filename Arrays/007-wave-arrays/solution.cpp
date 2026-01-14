f9eb23576d921427a9e2a2eb741ed92013b749e7
singhquitearyan <152776855+singhquitearyan@users.noreply.github.com>
2026-01-14 20:04:06 +0530

    Create solution.cpp

---
+++ Arrays/007-wave-arrays/solution.cpp
@@ -0,0 +1,14 @@
+//Author: Aryan Singh
+//Date: 14/01/26
+//Description:
+
+class Solution {
+  public:
+    void sortInWave(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        for( int i = 0; i<(n-1); i+= 2){
+            swap(arr[i], arr[i+1]);
+        }
+    }
+};
