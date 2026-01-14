6ed0dc258dbc5cefd4298b4d0b915dc18c957b7b
singhquitearyan <152776855+singhquitearyan@users.noreply.github.com>
2026-01-14 20:02:22 +0530

    Create solution.cpp

---
+++ Arrays/006-move-all-zeros -to-end/solution.cpp
@@ -0,0 +1,23 @@
+//Author: Aryan SIngh
+//Date: 14/01/2026
+//Description:
+
+class Solution {
+  public:
+    void pushZerosToEnd(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        int pos = 0;
+        for( int i = 0; i<n; i++){
+            if( arr[i] != 0) {
+                arr[pos] = arr[i];
+                pos++; 
+            }
+            
+        }
+        while( pos < n){
+            arr[pos] = 0;
+            pos++;
+        }
+    }
+};
