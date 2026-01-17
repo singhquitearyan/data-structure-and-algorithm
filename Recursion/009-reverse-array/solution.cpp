5107cd14a080fd28c92688e126e8f322042b69c9
singhquitearyan <152776855+singhquitearyan@users.noreply.github.com>
2026-01-17 18:56:04 +0530

    Update solution.cpp

--- Recursion/009-reverse-array/solution.cpp
+++ Recursion/009-reverse-array/solution.cpp
@@ -1,26 +1,30 @@
-#include<bits/stdc++.h>
+//Author: Aryan Singh
+//Date: 17/01/16
+//Description: we are using the most optimized way here to swap the arrrays, same way we can also swap the stings.
+
+#include <iostream>
 using namespace std;
-void arrayrev( vector<int>& arr, int i){
-    int n = arr.size();
 
-    if( i >= n-i-1)
-    return;
+void reverseArray(int arr[], int left, int right) {
+    // Base case
+    if (left >= right)
+        return;
 
-    swap(arr[i], arr[n-i-1]);
-    arrayrev( arr, i+1);
+    // Swap left and right elements
+    swap(arr[left], arr[right]);
+
+    // Move pointers inward
+    reverseArray(arr, left + 1, right - 1);
 }
-int main(){
-    int n;
-    cin >> n;
-    vector<int> arr(n);
-    for(int i = 0; i<n; i++){
-        cin >> arr[i];
-    }
 
-    arrayrev(arr, 0);
-    for(int i = 0; i<n; i++){
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    reverseArray(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
-    }
-    cout << endl;
-return 0;
+
+    return 0;
 }
