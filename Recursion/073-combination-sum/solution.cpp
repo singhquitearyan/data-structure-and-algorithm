21ba1779a139315fc3548306bf23ff0b48750227
singhquitearyan <152776855+singhquitearyan@users.noreply.github.com>
2026-02-01 22:02:58 +0530

    Implement combination sum algorithm using backtracking

---
+++ Recursion/073-combination-sum/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<int>temp;
+        backtrack(candidates, 0, target, temp);
+        return ans;
+    }
+
+    void backtrack(vector<int>& candidates, int index, int target, vector<int>& temp){
+        if(target == 0){
+            ans.push_back(temp);
+            return;
+        }
+
+        for(int i = index; i < candidates.size(); i++){
+            if( i > index && candidates[i] == candidates[i-1])
+            continue;
+
+            if(candidates[i] > target)
+            break;
+
+            temp.push_back(candidates[i]);
+            backtrack(candidates, i+1, target - candidates[i], temp);
+            temp.pop_back();
+        }
+    }
+};
